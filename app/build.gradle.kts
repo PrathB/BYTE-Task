d94fb7774bc5d03d82381883a70d823d9d29f1d2
Pratham Batra <pratham11.batra@gmail.com>
2024-09-11 20:56:50 +0530

    Added permission handling for user location (using PermissionX)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,9 +34,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    implementation("com.guolindev.permissionx:permissionx:1.7.1")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/test/bytetask/MainActivity.kt
+++ app/src/main/java/com/test/bytetask/MainActivity.kt
@@ -1,24 +1,79 @@
 package com.test.bytetask
 
-import android.graphics.BlurMaskFilter
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.location.LocationManager
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.permissionx.guolindev.PermissionX
+import com.test.bytetask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val byteLogo: TextView = findViewById(R.id.byte_logo)
-        byteLogo.setShadowLayer(
+        binding?.byteLogo?.setShadowLayer(
             15f,
             0f,
             0f,
             Color.parseColor("#66FF00")
         )
+
+        binding?.flStart?.setOnClickListener {
+            PermissionX.init(this)
+                .permissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .onForwardToSettings { scope, deniedList ->
+                    scope.showForwardToSettingsDialog(deniedList, "Please grant permission to access" +
+                            " precise location in App Settings", "OK", "Cancel")
+                }
+                .request { allGranted, _, deniedList ->
+                    if (allGranted) {
+                        if(isLocationEnabled()){
+                            binding?.byteLogo?.text="Location Enabled"
+                        }
+                        else{
+                            showEnableLocationDialog()
+                        }
+                    }else{
+                        Toast.makeText(this, "Permission Denied: $deniedList", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
     }
+    private fun isLocationEnabled() : Boolean{
+        val locationManager : LocationManager = getSystemService(Context.LOCATION_SERVICE)
+                as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+    private fun showEnableLocationDialog() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Location Services Disabled")
+        alertDialog.setMessage("Please enable location services to use this feature.")
+        alertDialog.setPositiveButton("Enable") { _, _ ->
+            // Open the device settings to enable location
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+        }
+        alertDialog.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        alertDialog.show()
+    }
+
 }
