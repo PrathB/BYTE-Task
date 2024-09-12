321bd60b10b3806ca764ee1ba7e7c6fccc9bf64e
Pratham Batra <pratham11.batra@gmail.com>
2024-09-12 11:55:27 +0530

    Added functions to track location

--- app/src/main/java/com/test/bytetask/MainActivity.kt
+++ app/src/main/java/com/test/bytetask/MainActivity.kt
@@ -1,30 +1,44 @@
 package com.test.bytetask
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 
 import android.graphics.Color
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Granularity
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.permissionx.guolindev.PermissionX
 import com.test.bytetask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding : ActivityMainBinding? = null
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         binding?.byteLogo?.setShadowLayer(
             15f,
@@ -44,6 +58,7 @@ class MainActivity : AppCompatActivity() {
                     if (allGranted) {
                         if(isLocationEnabled()){
                             binding?.byteLogo?.text="Location Enabled"
+                            startLocationTracking()
                         }
                         else{
                             showEnableLocationDialog()
@@ -66,7 +81,6 @@ class MainActivity : AppCompatActivity() {
         alertDialog.setTitle("Location Services Disabled")
         alertDialog.setMessage("Please enable location services to use this feature.")
         alertDialog.setPositiveButton("Enable") { _, _ ->
-            // Open the device settings to enable location
             val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(settingsIntent)
         }
@@ -75,5 +89,40 @@ class MainActivity : AppCompatActivity() {
         }
         alertDialog.show()
     }
+        @SuppressLint("MissingPermission")
+    private fun startLocationTracking(){
+        val mLocationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,1000).apply {
+            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.lastLocation?.let { location ->
+                val latitude = location.latitude
+                val longitude = location.longitude
+                updateLocationInFirebase(latitude, longitude)
+//                sendLocationToSocket(latitude, longitude)
+            }
+        }
+    }
+
+    private fun updateLocationInFirebase(lat: Double, lon: Double) {
+//        val database = FirebaseDatabase.getInstance()
+//        val userLocationRef = database.getReference("user_locations").child("userId")
+//        userLocationRef.setValue(UserLocation(lat, lon))
+            val locObj = UserLocation(lat,lon)
+            Log.i("lat:",locObj.latitude.toString())
+            Log.i("lon:",locObj.longitude.toString())
+            Log.i("timestamp:",locObj.timestamp.toString())
+    }
+
+    private fun sendLocationToSocket(lat: Double, lon: Double) {
+        // Socket connection code to send location data for live tracking
+    }
+
 
 }
